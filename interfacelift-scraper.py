31a7acb8f89a87d6e89834629c1517b7222c7478
Benjamin Heng <benjheng@gmail.com>
2014-12-20 00:29:25 +0800

    Fixed file deletion upon failed download

--- interfacelift-scraper.py
+++ interfacelift-scraper.py
@@ -1,18 +1,17 @@
 import os, re, urllib2
 
-scrapeUrl = "http://interfacelift.com/wallpaper/downloads/date/widescreen_16:9/1920x1080/"
+scrapeUrl = 'http://interfacelift.com/wallpaper/downloads/date/widescreen_16:9/1920x1080/'
 host = 'http://interfacelift.com'
 saveDir = 'scraped'
 
 # how many you want to download, 0 to download all
 goal = 0
-done = 0
+
 # go to your scrape url, note down how many pages there are. this should not exceed the max.
-maxPages = 209
+maxPages = 240
 
 # hi guy from the future, if this script doesn't work, you probably need to update the regex.
-tag = re.compile(r"<a href=\"(?P<url>.+)\"><img.+?src=\"/img_NEW/button_download")
-
+tag = re.compile(r'<a href=\"(?P<url>.+)\"><img.+?src=\"/img_NEW/button_download')
 
 if not os.path.exists(saveDir) :
     os.makedirs(saveDir)
@@ -27,8 +26,9 @@ if goal == 0:
     goal = goal - (goal % 100)
     goalStr = 'about %s' % str(goal)
 
+done = 1
 for page in range(1, maxPages+1):
-    print 'opening page %s of %s' % (page, maxPages)
+    print '[+] opening page %s of %s' % (page, maxPages)
     
     url = scrapeUrl + "index" + str(page)
 
@@ -40,9 +40,9 @@ for page in range(1, maxPages+1):
         f = urllib2.urlopen(req)
 
     except IOError, e:
-        print "Failed to open", url
+        print 'Failed to open', url
         if hasattr(e, 'code'):
-            print "Error code:", e.code
+            print 'Error code:', e.code
         continue
 
     html = f.read()
@@ -50,8 +50,9 @@ for page in range(1, maxPages+1):
     links = tag.finditer(html)
 
     for i in links:
-        if goal == 0 or done < goal :
-            print "scraping " + str(done + 1) + " of " + goalStr
+        failed = False
+        if goal == 0 or done <= goal :
+            print '[-] scraping %d of %s' % (done, goalStr)
             dlurl = host + i.group('url')
             req = urllib2.Request(dlurl, None, headers)
             saveFile = os.path.join(saveDir, str(done)+'.jpg')
@@ -61,8 +62,9 @@ for page in range(1, maxPages+1):
                     f.write(res.read())
                 except:
                     print 'failed'
-                    try:
-                        os.remove(saveFile)
-                    except: pass
+                    failed = True
+            if failed:
+                try: os.remove(saveFile)
+                except: pass
 
             done += 1
