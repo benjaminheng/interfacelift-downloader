3a1c9c9c435931f5e6c1356788ecd51295398ea3
Ben <benjheng@gmail.com>
2015-06-21 01:37:58 +0800

    Updated script with multithreading support

--- interfacelift-scraper.py
+++ interfacelift-scraper.py
@@ -1,71 +1,103 @@
 #!/usr/bin/python
-import os, re, urllib2
+import os
+import re
+import urllib2
+import threading
+import Queue
+import time
 
-scrapeUrl = 'http://interfacelift.com/wallpaper/downloads/date/widescreen_16:9/1920x1080/'
-host = 'http://interfacelift.com'
-saveDir = 'scraped'
-
-# how many you want to download, 0 to download all
-goal = 0
-
-# go to your scrape url, note down how many pages there are. this should not exceed the max.
-maxPages = 240
-
-# hi guy from the future, if this script doesn't work, you probably need to update the regex.
-tag = re.compile(r'<a href=\"(?P<url>.+)\"><img.+?src=\"/img_NEW/button_download')
-
-if not os.path.exists(saveDir) :
-    os.makedirs(saveDir)
-
-# pointless, just makes the output a little nicer.
-# formats goal if 0 to read 'about XX' 
-goalStr = str(goal)
-if goal == 0:
-    # round up maxPages*10 to next hundredth
-    # shitty convoluted code, i know, but works
-    goal = maxPages*10 + 99
-    goal = goal - (goal % 100)
-    goalStr = 'about %s' % str(goal)
-
-done = 1
-for page in range(1, maxPages+1):
-    print '[+] opening page %s of %s' % (page, maxPages)
-    
-    url = scrapeUrl + "index" + str(page)
-
+def download_file(url, saveDir):
     # interfacelift returns a 403 forbidden unless you include a referer.
     headers = { 'User-agent' : "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)",
                 'Referer': url}
+    req = urllib2.Request(url, None, headers)
+    filename = imgFilePattern.search(url).group()
+    saveFile = os.path.join(saveDir, filename+'.jpg')
+    with open(saveFile, 'wb') as f:
+        try:
+            res = urllib2.urlopen(req)
+            f.write(res.read())
+            print '[+] Downloaded %s' % filename
+        except Exception as e:
+            print e
+            try: os.remove(saveFile)
+            except: pass
+
+def download_worker():
+    while True:
+        url = queue.get()
+        download_file(url, saveDir)
+        queue.task_done()
+
+def get_page_path(pageNumber):
+    return '%sindex%d.html' % (resPath, pageNumber)
+
+def get_url_from_path(path):
+    return '%s/%s' % (host, path)
+
+def get_page_url(pageNumber):
+    return get_url_from_path(get_page_path(pageNumber))
+
+def has_next_page(pageContent, currentPage):
+    return True if pageContent.find(get_page_path(currentPage+ 1)) > -1 else False
+
+def open_page(pageNumber):
+    url = get_page_url(pageNumber)
+    # interfacelift returns a 403 forbidden unless you include a referer.
+    headers = { 'user-agent' : "mozilla/4.0 (compatible; msie 5.5; windows nt)",
+                'referer': url}
     try:
         req = urllib2.Request(url, None, headers)
         f = urllib2.urlopen(req)
-
     except IOError, e:
-        print 'Failed to open', url
+        print 'failed to open', url
         if hasattr(e, 'code'):
-            print 'Error code:', e.code
-        continue
+            print 'error code:', e.code
+    return f.read()
 
-    html = f.read()
+def pretty_time(seconds):
+    m, s = divmod(round(seconds), 60)
+    h, m = divmod(m, 60)
+    return "%d:%02d:%02d" % (h, m, s)
 
-    links = tag.finditer(html)
+resPath = '/wallpaper/downloads/date/widescreen_16:9/1920x1080/'
+host = 'http://interfacelift.com'
+saveDir = 'wallpapers'
 
-    for i in links:
-        failed = False
-        if goal == 0 or done <= goal :
-            print '[-] scraping %d of %s' % (done, goalStr)
-            dlurl = host + i.group('url')
-            req = urllib2.Request(dlurl, None, headers)
-            saveFile = os.path.join(saveDir, str(done)+'.jpg')
-            with open(saveFile, 'wb') as f:
-                try:
-                    res = urllib2.urlopen(req)
-                    f.write(res.read())
-                except:
-                    print 'failed'
-                    failed = True
-            if failed:
-                try: os.remove(saveFile)
-                except: pass
+# hi guy from the future, if this script doesn't work, you probably need to update the regex.
+tag = re.compile(r'<a href=\"(?P<path>.+)\"><img.+?src=\"/img_NEW/button_download')
+imgFilePattern = re.compile(r'[^/]*$')
 
-            done += 1
+# number of threads
+numThreads = 4
+
+if not os.path.exists(saveDir):
+    os.makedirs(saveDir)
+
+queue = Queue.Queue();
+timeStart = time.time()
+
+# Create threads
+for i in range(numThreads):
+    t = threading.Thread(target=download_worker)
+    t.daemon = True
+    t.start()
+
+# TODO: add urls to queue
+page = 1
+count = 0
+while True:
+    pageContent = open_page(page)
+    links = tag.finditer(pageContent)
+    for link in links:
+        queue.put(get_url_from_path(link.group('path')))
+        count += 1
+
+    if has_next_page(pageContent, page):
+        page += 1
+    else:
+        break
+
+queue.join() # block until all urls processed
+print '[*] Download finished! (%d files)' % count
+print '[*] Time taken: %s' % pretty_time(time.time() - timeStart)
